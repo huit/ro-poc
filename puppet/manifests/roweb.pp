09dc8bf76bf38fb7bf415b9438bcde33f39d515c
root <root@ip-10-0-1-131.ec2.internal>
2014-10-22 19:19:14 +0000

    Adding new Apache conf directory

--- puppet/manifests/roweb.pp
+++ puppet/manifests/roweb.pp
@@ -6,3 +6,11 @@ service { 'apache2':
   ensure => running,
   require => Package['apache2'],
 }
+
+package {'libapache2-mod-jk':
+  ensure => installed,
+}
+
+package {'libapache2-mod-auth-cas':
+  ensure => installed,
+}
